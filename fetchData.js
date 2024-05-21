4346400176e19606cdc133e807eccc9f61abb2b6
huevangxp <huevang933@gmail.comcd>
2024-05-21 09:59:14 +0700

    new

---
+++ fetchData.js
@@ -0,0 +1,20 @@
+const dataContainer = document.getElementById('data-container');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    // Process the fetched data
+    let content = ''; // Build the content to display
+
+    for (const item of data) {
+      // Example: Assuming data is an array of objects
+      content += `<p> name: ${item.name}</p>`;
+      content += `<p> username: ${item.username}</p>`;
+    }
+
+    dataContainer.innerHTML = content;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+    // Handle errors (optional: display an error message)
+  });
